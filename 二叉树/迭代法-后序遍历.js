0c82521f8ebe8c679b94a3aa6ef0c66036111060
web-wsr <2316516412@qq.com>
2025-02-06 23:10:12 +0800

    Update 迭代法-后序遍历.js
    update

--- 二叉树/迭代法-后序遍历.js
+++ 二叉树/迭代法-后序遍历.js
@@ -1,19 +1,20 @@
 function preOrderTraversal(root) {
-    const stack = [];
-    const res = [];
-    stack.push(root);
-    while (stack.length) {
-        let node = stack.pop();
-        // 中
-        if (node !== null) {
-            res.push(node.val);
-        } else {
-            continue;
-        }
-        // 左
-        stack.push(node.left);
-        // 右
-        stack.push(node.right);
+  // 迭代法
+  const stack = [];
+  const res = [];
+  stack.push(root);
+  while (stack.length) {
+    let node = stack.pop();
+    // 中
+    if (node !== null) {
+      res.push(node.val);
+    } else {
+      continue;
     }
-    return res.reverse();
+    // 左
+    stack.push(node.left);
+    // 右
+    stack.push(node.right);
+  }
+  return res.reverse();
 }
